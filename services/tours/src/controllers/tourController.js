8176814cd494a15784374e5b9744050949729f2a
Luka Maric <lukamaric718@gmail.com>
2026-05-30 09:12:49 +0200

    Expose tour lifecycle REST endpoints

--- services/tours/src/controllers/tourController.js
+++ services/tours/src/controllers/tourController.js
@@ -22,8 +22,62 @@ const getTourById = asyncHandler(async (req, res) => {
   res.status(200).json({ tour });
 });
 
+const addTransportTime = asyncHandler(async (req, res) => {
+  const transportTime = await tourService.addTransportTime(req.user.sub, req.params.id, req.body);
+
+  res.status(201).json({
+    message: "Transport time added successfully.",
+    transportTime
+  });
+});
+
+const listTransportTimes = asyncHandler(async (req, res) => {
+  const transportTimes = await tourService.listTransportTimes(req.user.sub, req.params.id);
+
+  res.status(200).json({ transportTimes });
+});
+
+const publishTour = asyncHandler(async (req, res) => {
+  const tour = await tourService.publishTour(req.user.sub, req.params.id);
+
+  res.status(200).json({
+    message: "Tour published successfully.",
+    tour
+  });
+});
+
+const archiveTour = asyncHandler(async (req, res) => {
+  const tour = await tourService.archiveTour(req.user.sub, req.params.id);
+
+  res.status(200).json({
+    message: "Tour archived successfully.",
+    tour
+  });
+});
+
+const activateTour = asyncHandler(async (req, res) => {
+  const tour = await tourService.activateTour(req.user.sub, req.params.id);
+
+  res.status(200).json({
+    message: "Tour activated successfully.",
+    tour
+  });
+});
+
+const listPublishedTours = asyncHandler(async (_req, res) => {
+  const tours = await tourService.listPublishedTours();
+
+  res.status(200).json({ tours });
+});
+
 module.exports = {
   createTour,
   getMyTours,
-  getTourById
+  getTourById,
+  addTransportTime,
+  listTransportTimes,
+  publishTour,
+  archiveTour,
+  activateTour,
+  listPublishedTours
 };
