84860200e909a379e9e538e23beba95414e41549
Luka Maric <lukamaric718@gmail.com>
2026-05-30 09:20:49 +0200

    Add tours gRPC service for lifecycle calls

--- services/tours/src/server.js
+++ services/tours/src/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const env = require("./config/env");
 const { connectDatabase } = require("./config/db");
+const { startGrpcServer } = require("./grpcServer");
 
 async function start() {
   try {
@@ -8,6 +9,7 @@ async function start() {
     app.listen(env.PORT, () => {
       console.log(`Tours service is running on port ${env.PORT}.`);
     });
+    startGrpcServer();
   } catch (error) {
     console.error("Failed to start tours service:", error);
     process.exit(1);
